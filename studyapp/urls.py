c805911a3f4d3aeba9be78b23d1c074451d97fa1
Your Name <you@example.com>
2022-07-25 16:38:51 +0530

    commit25/7/2022

---
+++ studyapp/urls.py
@@ -0,0 +1,48 @@
+from django.views import *
+from django.urls import  path
+from django.conf import settings
+from django.conf.urls.static import static
+from .import views
+from django.contrib.auth import views as auth_views
+from django.contrib import admin
+
+
+urlpatterns =[  path('',views.login,name='login'),
+path('base',views.base,name='base'),
+path('register',views.register,name='register'),
+path('index',views.index,name='index'),
+
+#ADMIN
+path('admin_head_dashboard',views.admin_head_dashboard,name='admin_head_dashboard'),
+path('admin_ithead_page',views.admin_ithead_page,name='admin_ithead_page'),
+path('admin_ithead_leads',views.admin_ithead_leads,name='admin_ithead_leads'),
+path('admin_councilor_page',views.admin_councilor_page,name='admin_councilor_page'),
+path('admin_councilor_leads',views.admin_councilor_leads,name='admin_councilor_leads'),
+path('admin_document_page',views.admin_document_page,name='admin_document_page'),
+path('admin_leads',views.admin_leads,name='admin_leads'),
+path('admin_documents_page_documents',views.admin_documents_page_documents,name='admin_documents_page_documents'),
+path('load_admin_client_currentstatus',views.load_admin_client_currentstatus,name='load_admin_client_currentstatus'),
+
+#IT HEAD
+path('IT_head_dashboard',views.IT_head_dashboard,name='IT_head_dashboard'),
+path('IT_head_leads',views.IT_head_leads,name='IT_head_leads'),
+
+
+#STUDENT COUNCILOR
+path('Student_head_dashboard',views.Student_head_dashboard,name='Student_head_dashboard'),
+path('Leads_Data_Collect',views.Leads_Data_Collect,name='Leads_Data_Collect'),
+
+
+#DOCUMENT OFFICIER
+path('Document_head_dashboard',views.Document_head_dashboard,name='Document_head_dashboard'),
+path('Document_head_Profile',views.Document_head_Profile,name='Document_head_Profile'),
+
+#Team_Lead
+path('Team_Lead_dashboard',views.Team_Lead_dashboard,name='Team_Lead_dashboard'),
+
+#STUDENT
+path('studeny_progress_report',views.studeny_progress_report,name='studeny_progress_report'),
+path('studeny_progress2_report',views.studeny_progress2_report,name='studeny_progress2_report'),
+
+ 
+]
---
+++ studyapp/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render
+
+def base(request):
+    return render(request,'base.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'register.html')
+
+
+def index(request):
+    return render(request,'index.html')
+
+#ADMIN
+
+def admin_head_dashboard(request):
+    return render(request,'admin_dashboard.html')
+
+def admin_ithead_page(request):  
+    return render(request,'admin_ithead_page.html') 
+def admin_ithead_leads(request):  
+    return render(request,'admin_ithead_leads.html')   
+def admin_councilor_page(request):  
+    return render(request,'admin_councilor_page.html')  
+def admin_document_page(request):  
+    return render(request,'admin_document_page.html')         
+def admin_councilor_leads(request):  
+    return render(request,'admin_councilor_leads.html')   
+def admin_documents_page_documents(request):  
+    return render(request,'admin_documents_page_documents.html')     
+def load_admin_client_currentstatus(request):
+    return render(request,'admin_client_status_current.html') 
+def admin_leads(request):
+    return render(request,'admin_leads.html')              
+
+
+
+#IT HEAD
+def IT_head_dashboard(request):
+    return render(request,'IT_head_dashboard.html')
+
+def IT_head_leads(request):
+    return render(request,'IT_head_leads.html')
+
+
+    
+#STUDENT COUNCILOR
+def Student_head_dashboard(request):
+    return render(request,'StudentCounsilor.html')
+
+def Leads_Data_Collect(request):
+    return render(request,'Leads_Data_Collects.html')
+
+
+#DOCUMENT OFFICIER
+def Document_head_dashboard(request):
+    return render(request,'Document_Officer_dashboard.html')
+
+def Document_head_Profile(request):
+    return render(request,'Document-profile.html')
+
+#TEAM LEAD
+
+def Team_Lead_dashboard(request):
+    return render(request, 'Team_Lead_dashboard.html')
+
+#STUDENT
+def studeny_progress_report(request):
+    return render(request,'studentProgress_Report.html')
+def studeny_progress2_report(request):
+    return render(request,'studentProgress2.html')
+
+
